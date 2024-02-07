fa238d0a90e48150a79d1f0ab66dc2f24eecee9a
Mirko Mollik <mirkomollik@gmail.com>
2024-02-07 21:19:55 +0100

    add doc to env files
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>

--- src/const.ts
+++ src/const.ts
@@ -1,7 +1,8 @@
+// recommened value from ebsi
 export const DEFAULT_NBHASHES = 27;
-
+// recommened value from ebsi
 export const DEFAULT_SIZE = 10;
-
+// recommened value from ebsi
 export const DEFAULT_EPOCH = 3600;
-
+// recommened value from ebsi
 export const DEFAULT_FALSE_POSITIVE = 1e-8;
